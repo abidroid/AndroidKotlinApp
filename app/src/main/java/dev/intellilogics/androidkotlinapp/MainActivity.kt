450d325bc254af69367c95ec47b030b4f5fa822d
Muhammad Abid <abidroid1@gmail.com>
2024-10-13 10:30:29 +0500

    main screen created

--- app/src/main/java/dev/intellilogics/androidkotlinapp/MainActivity.kt
+++ app/src/main/java/dev/intellilogics/androidkotlinapp/MainActivity.kt
@@ -1,20 +1,36 @@
 package dev.intellilogics.androidkotlinapp
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnCalculator: Button
+    private lateinit var btnImages: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        btnCalculator = findViewById(R.id.btn_calculator)
+        btnImages = findViewById(R.id.btn_images)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        btnCalculator.setOnClickListener{
+//            val intent = Intent(this, CalculatorActivity::class.java)
+//            startActivity(intent)
+
         }
     }
 }
